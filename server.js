323e47680e16a820d0101ad492fea0745afafb9f
adityapanther <raj68518@gmail.com>
2021-11-20 22:03:27 +0530

    way 2 added

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// ---------------------- way 1 added ----------------------------
 const net = require('net')
 
 const portList = [3000, 3001];
@@ -37,3 +38,43 @@ const server = net.createServer( (socket) => {
 server.listen( 4100, "127.0.0.1" ,() => {
     console.log(server.address());
 })
+
+
+//------------  way 2 added ------------------------------------
+
+
+// const net = require('net')
+
+// const portList = [3000, 3001];
+
+// let portIndex = 0;
+// let userNumber = 0;
+// function changePort(){
+//     const num = userNumber / 2
+//     if(num.toString().includes('.')){
+//         portIndex = 0;
+//     }else{
+//         portIndex = 1;
+//     }
+//     userNumber++;
+// }
+// const server = net.createServer( (socket) => {
+//     socket.once('data', (msg) => {
+//         changePort()
+//         var serviceSocket = new net.Socket();
+//         serviceSocket.connect(parseInt(portList[portIndex]), '127.0.0.1', () => {
+//             serviceSocket.write(msg);
+//         })
+//         socket.pipe(serviceSocket);
+//         serviceSocket.pipe(socket);
+        
+//     })
+
+// }).on('error', (err) => {
+//     console.log(err);
+//     throw err
+// })
+
+// server.listen( 4100, "127.0.0.1" ,() => {
+//     console.log(server.address());
+// })
